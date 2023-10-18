7d7782db4866a928dc1e0927c5206c708a23873b
Momin Hayat Khan <mominhyatkhandeveloper@gmail.com>
2023-10-18 12:26:33 +0500

    3rd commit socket connection and minor errors 10/18/2023

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const io = require("socket.io")(process.env.SOCKET_PORT || 8080, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
@@ -22,6 +23,53 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+//Socket.io
+let users = [];
+io.on("connection", (socket) => {
+  console.log(`Got a connection! @ ${socket.id}`);
+  socket.on("addUser", (userId) => {
+    const isUserExist = users.find((user) => user.userId === userId);
+    if (!isUserExist) {
+      const user = { userId, socketId: socket.id };
+      users.push(user);
+      io.emit("getUsers", users);
+    }
+  });
+
+  socket.on(
+    "sendMessage",
+    async ({ senderId, receiverId, message, conversationId }) => {
+      const receiver = users.find((user) => user.userId === receiverId);
+      const sender = users.find((user) => user.userId === senderId);
+      const user = await Users.findById(senderId);
+      if (receiver) {
+        io.to(receiver.socketId)
+          .to(sender.socketId)
+          .emit("getMessage", {
+            senderId,
+            message,
+            conversationId,
+            receiverId,
+            user: { id: user._id, fullName: user.fullName, email: user.email },
+          });
+      } else {
+        io.to(sender.socketId).emit("getMessage", {
+          senderId,
+          message,
+          conversationId,
+          receiverId,
+          user: { id: user._id, fullName: user.fullName, email: user.email },
+        });
+      }
+    }
+  );
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.socketId !== socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 
